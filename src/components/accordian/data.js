1f0fea3e231f3b3749f9433f0b444050fd804178
hanzlikhan <khangormani79@gmail.com>
2024-09-29 21:48:57 +0500

    init

---
+++ src/components/accordian/data.js
@@ -0,0 +1,23 @@
+const data = [
+  {
+    id : "1",
+    question: "what are accordion components?",
+    answer: "Accordion components are user interface elements used for organ"
+  },
+  {
+    id : "2",
+    question: "why are they used for?",
+    answer: "They are commonly employed in the various contents including FAQS, prduct discriptio"
+  },
+  {
+    id: "3",
+    question: "Accordion as a musical instrument",
+    answer : " Accordion is a musical instrument with keyboard and bellows. It produces sound"
+  },
+  {
+    id: "4",
+    question: "can i create an accodion components with a different framework?",
+    answer: "yes of course , it is very possible to create accordion components"
+  }
+];
+export default data;
